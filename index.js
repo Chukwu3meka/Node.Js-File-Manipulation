37477384b07ca8d3f7de6553340c4cfb043734a1
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-01-13 10:30:59 +0100

    deleted unused code in index.js

--- index.js
+++ index.js
@@ -3,23 +3,4 @@ const fileRename = require("./fileRename");
 // uncomment this line to run the file rename handler
 fileRename();
 
-// const fs = require("fs");
-// const dirPath = "./division";
-// const rename = require("fs").rename;
-
-// async function ls(path) {
-//   const dir = await fs.promises.opendir(path);
-//   for await (const dirent of dir) {
-//     console.log(dirent.name);
-//     // if (playersArray[dirent.name]) {
-//     //   rename(`${dir}/${dirent.name}`, `${dir}/${playersArray[dirent.name]}.webp`, (err) => {
-//     //     if (err) console.log(err);
-//     //   });
-
-//     // console.log(dirent.name, playersArray[dirent.name]);
-//   }
-// }
-
-// ls(dirPath).catch(console.error);
-
-// return "success";
+return "success";
